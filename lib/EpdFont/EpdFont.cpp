0a30da6303b6f5e5413a85b457d7c4d4acacb81b
ztrawhcs <matthew.s.schwartz@gmail.com>
2026-02-13 17:05:40 -0500

    Update EpdFont.cpp

--- lib/EpdFont/EpdFont.cpp
+++ lib/EpdFont/EpdFont.cpp
@@ -1,9 +1,10 @@
 #include "EpdFont.h"
-#include "EpdFontFamily.h"
 
 #include <Utf8.h>
 
 #include <algorithm>
+
+#include "EpdFontFamily.h"
 
 void EpdFont::getTextBounds(const char* string, const int startX, const int startY, int* minX, int* minY, int* maxX,
                             int* maxY) const {
@@ -35,13 +36,13 @@ void EpdFont::getTextBounds(const char* string, const int startX, const int star
     *maxX = std::max(*maxX, cursorX + glyph->left + glyph->width);
     *minY = std::min(*minY, cursorY + glyph->top - glyph->height);
     *maxY = std::max(*maxY, cursorY + glyph->top);
-    
+
     cursorX += glyph->advanceX;
-    
+
     // CUSTOM TRACKING: If we are in forced bold mode, reduce letter spacing by 1px
     // We explicitly exclude normal spaces (' ') and non-breaking spaces (0x00A0)
     if (EpdFontFamily::globalForceBold && cp != ' ' && cp != 0x00A0) {
-        cursorX -= 1;
+      cursorX -= 1;
     }
   }
 }
